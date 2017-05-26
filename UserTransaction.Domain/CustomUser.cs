2d9d817e6a5a8ee45b18dd746a20d11fb1d42802
Jysan Aziz <jeexan2@gmail.com>
2017-05-27 01:24:02 +0600

    Third Commit

---
+++ UserTransaction.Domain/CustomUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserTransaction.Domain
+{
+    public class CustomUser
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public Decimal Amount { get; set; }
+    }
+}
---
+++ UserTransaction.Domain/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserTransaction.Domain
+{
+    public class Transaction: Base
+    {
+        public decimal Amount { get; set; }
+        public DateTime DateTransaction { get; set; }
+        public int UserId { get; set; }
+        public Transaction()
+        {
+            
+        }
+    }
+}
